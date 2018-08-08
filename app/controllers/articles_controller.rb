be2219fb6d6934e673ed9fe01cfa3e53731f9a96
AdGlad <bagladstone@bigpond.com>
2018-08-08 07:18:07 +0000

    Added form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   
   def index
    @articles = Article.all
@@ -19,16 +20,22 @@ class ArticlesController < ApplicationController
   end
 
   def show
-  @article=Article.find(params[:id])
 
   end
 
   def edit
-    @article=Article.find(params[:id])
+  end
+
+  def destroy
+    if @article.destroy
+    flash[:notice] = "Article successfully deleted"
+    redirect_to articles_path
+    else
+     render 'new'
+   end
   end
 
   def update
-   @article = Article.find(params[:id])
    if @article.update(article_params)
      flash[:notice] = "Article successfully updated"
      redirect_to article_path(@article)
@@ -39,6 +46,9 @@ class ArticlesController < ApplicationController
   private
   def article_params
    params.require(:article).permit(:title, :description)
+  end
+  def set_article
+    @article=Article.find(params[:id])
   end
 
 end
