88f69da2007e546aef061bfd0b721a297c41c0c7
AdGlad <bagladstone@bigpond.com>
2018-08-08 06:39:26 +0000

    Edit,show pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  
+  def index
+   @articles = Article.all
+  end
+
   def new
    @article = Article.new
   end
@@ -12,9 +17,24 @@ class ArticlesController < ApplicationController
      render 'new'
    end
   end
+
   def show
   @article=Article.find(params[:id])
 
+  end
+
+  def edit
+    @article=Article.find(params[:id])
+  end
+
+  def update
+   @article = Article.find(params[:id])
+   if @article.update(article_params)
+     flash[:notice] = "Article successfully updated"
+     redirect_to article_path(@article)
+   else
+     render 'new'
+   end
   end
   private
   def article_params
